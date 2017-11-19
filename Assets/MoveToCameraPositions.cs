548fed7c3da8ef365c62306f8d9a17aa69161732
Sean Farrell <sfarrell@backflipstudios.com>
2017-11-19 15:50:27 -0700

    Added some new positions that are behind the camera for objects to move towards. Added a line that moves towards a random positions (Works really well!) Good experiment.

---
+++ Assets/MoveToCameraPositions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToCameraPositions : MonoBehaviour {
+
+    // Use this for initialization
+    private GameObject p1, p2, p3, p4;
+    private List<GameObject> positions = new List<GameObject>();
+    private GameObject direction;
+    public float speed;
+
+    void Start () {
+        if (p1 == null)
+        {
+            p1 = GameObject.FindGameObjectWithTag("p1");
+            positions.Add(p1);
+        }
+        if (p2 == null)
+        {
+            p2 = GameObject.FindGameObjectWithTag("p2");
+            positions.Add(p2);
+        }
+        if (p3 == null)
+        {
+            p3 = GameObject.FindGameObjectWithTag("p3");
+            positions.Add(p3);
+        }
+        if (p4 == null)
+        {
+            p4 = GameObject.FindGameObjectWithTag("p4");
+            positions.Add(p4);
+        }
+
+        int p = Random.Range(0, 4);
+        direction = positions[p];
+	}
+	
+    void Update()
+    {
+        MoveToCameraPosition();
+    }
+
+    void MoveToCameraPosition()
+    {
+        transform.position = Vector3.Lerp(transform.position, direction.transform.position, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/SpawnLine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLine : MonoBehaviour {
+
+    public GameObject Line;
+    public float cd;
+    private float t;
+    // Use this for initialization
+    void Start()
+    {
+        t = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (t > cd)
+        {
+            SpawnLines();
+            t = 0;
+        }
+        else
+        {
+            t += Time.deltaTime;
+        }
+    }
+
+    void SpawnLines()
+    {
+        Instantiate(Line, transform.position, transform.rotation);
+    }
+}
+
